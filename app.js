df474d148b7e5fdfdb581ef285d3e7d1f6f2a133
christiansanchez-SE <christiansanchez714@gmail.com>
2024-04-25 23:11:28 -0400

    new commit

--- app.js
+++ app.js
@@ -1,46 +1,42 @@
-let character = document.getElementById("character");
-let block = document.getElementById("block");
+const character = document.getElementById("character");
+const block = document.getElementById("block");
+const playerScoreDisplay = document.getElementById("playerScore");
+const jumpButton = document.getElementById("jumpButton");
 
-function jump () {
-    if (character.classList != "animate"){
-        character.classList.add("animate");
-    }
-    setTimeout (function() {
-        character.classList.remove("animate")
-    }, 500);    
+let playerScore = 0;
+
+function updateScoreDisplay() {
+  playerScoreDisplay.textContent = playerScore;
 }
 
-let checkDead = setInterval (function(){
 
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft < 76 && blockLeft > 0 && characterTop >= 231){
-        block.style.animation = "none";
-        // block.style.display = "none";
-        alert("You Lose!");
-        location.reload();
+function jump() {
+    if (!character.classList.contains("animate")) {
+      character.classList.add("animate");
+      setTimeout(function () {
+        character.classList.remove("animate");
+      }, 500);
     }
-}, 10);
+  }
 
-// Pausing the game
+  jumpButton.addEventListener("click", jump);
 
-// let isPaused = false;
+  let checkCollision = setInterval(function () {
+    let characterTop = parseInt(
+      window.getComputedStyle(character).getPropertyValue("top")
+    );
+    let blockLeft = parseInt(
+      window.getComputedStyle(block).getPropertyValue("left")
+    );
+  
+    if (blockLeft < 76 && blockLeft > 0 && characterTop >= 200) {
+      block.style.animation = "none";
+      alert("Game Over!");
+      location.reload();
+    } else if (blockLeft < 0) {
+      playerScore++;
+      updateScoreDisplay();
+    }
+  }, 10);
 
-// document.addEventListener('keyup', function(e){
-//     if(e.which === 32)
-//     {
-//         if(isPaused) resumeGame();
-//         else pauseGame();
-//     }
-// });
-
-// function pauseGame(){
-//     clearInterval(interval);
-//     isPaused = true;
-// }
-
-// function resumeGame(){
-//     isPaused = false;
-//     interval = setInterval(runGame,20);
-// }
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
